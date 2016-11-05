dc094d9fbbe0038d91fb77235853584b234c78dc
fabiopulvi <fabio.pulvirenti88@gmail.com>
2016-11-05 15:30:42 +0100

    added first reduce phase

--- src/main/java/pampa/pampa_main.java
+++ src/main/java/pampa/pampa_main.java
@@ -4,13 +4,15 @@ package pampa; /**
 
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.api.java.function.*;
+import org.apache.spark.util.SystemClock;
 import scala.Tuple2;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class pampa_main {
@@ -40,6 +42,78 @@ public class pampa_main {
         }
         JavaRDD<Tuple2<String,ArrayList<Integer>>> dataset_2 = dataset.map(new split());
 
+        //The dataset is in the right format now
+/* questa non andava bene perchè non potevo cambiare tipo di output.
+        //let's map it to build the first level projected tables in the reducers
+        class mapping implements FlatMapFunction<Tuple2<String,ArrayList<Integer>>,Tuple2<Integer, Tuple2<String,ArrayList<Integer>>>> {
+            public Iterable<Tuple2<Integer, Tuple2<String,ArrayList<Integer>>>> call (Tuple2<String,ArrayList<Integer>> row_input) {
+                List<Tuple2<Integer, Tuple2<String,ArrayList<Integer>>>> row_output = new ArrayList<Tuple2<Integer, Tuple2<String,ArrayList<Integer>>>>();
+                for (int tid=0;tid<row_input._2().size();tid++){
+                    String partial_tidlist="";
+                    int key=row_input._2().get(tid);
+                    ArrayList<Integer> tidlist_temp = new ArrayList<Integer>();
+                    for (int i=tid+1;i<row_input._2().size();i++)
+                        tidlist_temp.add(row_input._2().get(i));
+                    Tuple2 <String, ArrayList<Integer>> value = new Tuple2<String, ArrayList<Integer>>(row_input._1(),tidlist_temp);
+                    Tuple2<Integer,Tuple2<String, ArrayList<Integer>>> tupla = new Tuple2<Integer, Tuple2<String, ArrayList<Integer>>>(key,value);
+                    row_output.add(tupla);
+                }
+                return row_output;
+
+            }
+        }
+       */
+
+        //let's map it to build the first level projected tables in the reducers
+        class mapping2 implements PairFlatMapFunction<Tuple2<String,ArrayList<Integer>>,Integer, Tuple2<String,ArrayList<Integer>>> {
+            public Iterable<Tuple2<Integer, Tuple2<String,ArrayList<Integer>>>> call (Tuple2<String,ArrayList<Integer>> row_input) {
+                List<Tuple2<Integer, Tuple2<String,ArrayList<Integer>>>> row_output = new ArrayList<Tuple2<Integer, Tuple2<String,ArrayList<Integer>>>>();
+                for (int tid=0;tid<row_input._2().size();tid++){
+                    String partial_tidlist="";
+                    int key=row_input._2().get(tid);
+                    ArrayList<Integer> tidlist_temp = new ArrayList<Integer>();
+                    for (int i=tid+1;i<row_input._2().size();i++)
+                        tidlist_temp.add(row_input._2().get(i));
+                    Tuple2 <String, ArrayList<Integer>> value = new Tuple2<String, ArrayList<Integer>>(row_input._1(),tidlist_temp);
+                    Tuple2<Integer,Tuple2<String, ArrayList<Integer>>> tupla = new Tuple2<Integer, Tuple2<String, ArrayList<Integer>>>(key,value);
+                    row_output.add(tupla);
+                }
+                return row_output;
+
+            }
+        }
+
+        JavaRDD<Tuple2<Integer, Tuple2<String,ArrayList<Integer>>>> mapped_values = dataset_2.flatMap(new mapping());
+        JavaPairRDD<Integer, Tuple2<String,ArrayList<Integer>>> mapped_values2 = dataset_2.flatMapToPair(new mapping2());
+
+        List<Tuple2<String,ArrayList<Integer>>> initialSet=  new ArrayList<Tuple2<String, ArrayList<Integer>>>();
+
+        class addtolist implements Function2<List<Tuple2<String,ArrayList<Integer>>>, Tuple2<String,ArrayList<Integer>>, List<Tuple2<String,ArrayList<Integer>>>> {
+            public List<Tuple2<String,ArrayList<Integer>>> call(List<Tuple2<String,ArrayList<Integer>>> list, Tuple2<String,ArrayList<Integer>> s) {
+                list.add(s);
+                return list;
+            };
+        }
+
+        class mergelist implements Function2 <List<Tuple2<String,ArrayList<Integer>>>, List<Tuple2<String,ArrayList<Integer>>>, List<Tuple2<String,ArrayList<Integer>>>> {
+            public  List<Tuple2<String,ArrayList<Integer>>> call(List<Tuple2<String,ArrayList<Integer>>> list, List<Tuple2<String,ArrayList<Integer>>> list2) {
+                list.addAll(list2);
+                return list;
+            }
+        }
+
+
+
+
+        JavaPairRDD<Integer, List<Tuple2<String,ArrayList<Integer>>>> reduced_values = mapped_values2.aggregateByKey(initialSet,new addtolist(), new mergelist());
+
+
+
+
+
+
+
+
 
 
         class GetLength implements Function<String, Integer> {
@@ -54,9 +128,24 @@ public class pampa_main {
             }});
         dataset_2.foreach(new VoidFunction<Tuple2<String,ArrayList<Integer>>>(){
             public void call(Tuple2<String,ArrayList<Integer>> t) {
-                System.out.println(t._1());
+                System.out.println(t._1()+"--"+t._2());
 
             }});
+        mapped_values.foreach(new VoidFunction<Tuple2<Integer, Tuple2<String, ArrayList<Integer>>>>() {
+            public void call(Tuple2<Integer, Tuple2<String, ArrayList<Integer>>> integerTuple2Tuple2) throws Exception {
+                System.out.println(integerTuple2Tuple2._1()+" "+integerTuple2Tuple2._2()._1()+":"+integerTuple2Tuple2._2()._2());
+
+            }
+        });
+
+        reduced_values.foreach(new VoidFunction<Tuple2<Integer, List<Tuple2<String, ArrayList<Integer>>>>>() {
+            public void call(Tuple2<Integer, List<Tuple2<String, ArrayList<Integer>>>> integerListTuple2) throws Exception {
+                System.out.println("key: "+integerListTuple2._1());
+                for (Tuple2<String, ArrayList<Integer>> tupla: integerListTuple2._2()) {
+                    System.out.println(tupla._1()+":"+tupla._2());
+                }
+            }
+        });
         //dataset.collect().foreach(println);
         System.out.println(lineLengths);
 
